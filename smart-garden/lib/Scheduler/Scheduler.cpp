b008fd5041958525230c5478a4293a830fe279ab
Thái <62926491+nhthai173@users.noreply.github.com>
2024-06-24 16:47:44 +0700

    chore: Add Scheduler library

---
+++ smart-garden/lib/Scheduler/Scheduler.cpp
@@ -0,0 +1,338 @@
+#pragma once
+
+#include "Scheduler.hpp"
+
+template<typename T>
+Scheduler<T>::Scheduler(NTPClient *timeClient) {
+    this->timeClient = timeClient;
+    SCHEDULE_FS.begin(true);
+
+    // Load tasks from file
+    load();
+}
+
+template<typename T>
+Scheduler<T>::~Scheduler() {
+    tasks.clear();
+    closeFile();
+    SCHEDULE_FS.end();
+}
+
+template<typename T>
+void Scheduler<T>::openFile(bool overwrite, uint8_t mode) {
+#if defined(ESP8266)
+    if (file && file.isFile()) {
+        file.close();
+    }
+    if (overwrite) {
+        this->file = SCHEDULE_FS.open(filePath, "w+");
+    } else {
+        this->file = SCHEDULE_FS.open(filePath, "a+");
+    }
+#elif defined(ESP32)
+    String modeStr;
+    if (mode == WRITE_ONLY) {
+        modeStr = FILE_WRITE;
+    } else if (mode == APPEND) {
+        modeStr = FILE_APPEND;
+    } else if (mode == READ_ONLY) {
+        modeStr = FILE_READ;
+    }
+    if (file) {
+        file.close();
+    }
+    if (!SCHEDULE_FS.exists(filePath)) {
+        file = SCHEDULE_FS.open(filePath, modeStr.c_str(), true);
+        return;
+    }
+    if (overwrite) {
+        file = SCHEDULE_FS.open(filePath, FILE_WRITE, false);
+    } else {
+        file = SCHEDULE_FS.open(filePath, modeStr.c_str(), false);
+    }
+#endif
+}
+
+template<typename T>
+void Scheduler<T>::closeFile() {
+    if (this->file) {
+        this->file.close();
+    }
+}
+
+template<typename T>
+bool Scheduler<T>::writeTaskToFile(schedule_task_t<T> *task) {
+    openFile(false, APPEND);
+    if (!file || file.name() == nullptr) {
+        Serial.println("Failed to open file for writing");
+        return false;
+    }
+
+    file.printf("%d|%d|%d|%d%d%d%d%d%d%d|%s|%d|%d\n",
+                task->id,
+                task->time.hour,
+                task->time.minute,
+                task->repeat.monday,
+                task->repeat.tuesday,
+                task->repeat.wednesday,
+                task->repeat.thursday,
+                task->repeat.friday,
+                task->repeat.saturday,
+                task->repeat.sunday,
+                task->args->toString().c_str(),
+                task->enabled,
+                task->executed);
+
+    closeFile();
+    return true;
+}
+
+template<typename T>
+schedule_task_t<T> Scheduler<T>::parseTask(const String &task) {
+    schedule_task_t<T> result{};
+    result.id = 0;
+    result.args = new T();
+    uint8_t count = 0;
+    int last_pos = 0;
+    std::vector<uint8_t> values;
+    String repeat = "";
+    String args = "";
+
+    // Task format: id|hour|minute|repeat|args|enabled|executed
+
+    while ((unsigned int) last_pos < task.length()) {
+        int pos = task.indexOf("|", last_pos);
+        if (pos < 0) pos = task.length();
+
+        // repeat part
+        if (count == 3) {
+            repeat = task.substring(last_pos, pos);
+        }
+        // args part
+        else if (count == 4) {
+            args = task.substring(last_pos, pos);
+        }
+        // other parts
+        else {
+            uint8_t num = (uint8_t) task.substring(last_pos, pos).toInt();
+            values.push_back(num);
+        }
+        last_pos = pos + 1;
+        ++count;
+    }
+
+    if (values.size() != 5 || repeat == "" || args == "")
+    {
+        Serial.println("Invalid task format");
+        return result;
+    }
+
+    // Parse repeat days
+    result.repeat.monday = repeat.charAt(0) == '1';
+    result.repeat.tuesday = repeat.charAt(1) == '1';
+    result.repeat.wednesday = repeat.charAt(2) == '1';
+    result.repeat.thursday = repeat.charAt(3) == '1';
+    result.repeat.friday = repeat.charAt(4) == '1';
+    result.repeat.saturday = repeat.charAt(5) == '1';
+    result.repeat.sunday = repeat.charAt(6) == '1';
+
+    // Parse args
+    result.args->parse(args);
+
+    // Assign values
+    result.id = values[0];
+    result.time.hour = values[1];
+    result.time.minute = values[2];
+    result.enabled = values[3];
+    result.executed = values[4];
+
+    return result;
+}
+
+template<typename T>
+void Scheduler<T>::setCallback(std::function<void(schedule_task_t<T>)> callback) {
+    this->callback = std::move(callback);
+}
+
+template<typename T>
+bool Scheduler<T>::load() {
+    openFile(false, READ_ONLY);
+    if (!file || file.name() == nullptr) {
+        Serial.println("Failed to open file for reading");
+        return false;
+    }
+
+    // Clear tasks
+    tasks.clear();
+
+    // Read file
+    if (file.size() == 0) {
+        closeFile();
+        return true;
+    }
+
+    String line;
+    while (file.available()) {
+        line = file.readStringUntil('\n');
+        if (line.length() > 0) {
+            auto task = parseTask(line);
+            if (task.id)
+                tasks.push_back(task);
+        }
+    }
+
+    closeFile();
+    return true;
+}
+
+template<typename T>
+bool Scheduler<T>::save() {
+    openFile(true, WRITE_ONLY);
+    if (!file || file.name() == nullptr) {
+        Serial.println("Failed to open file for writing");
+        return false;
+    }
+    closeFile();
+
+    for (auto &task: tasks) {
+        writeTaskToFile(&task);
+    }
+
+    return true;
+}
+
+template<typename T>
+bool Scheduler<T>::addTask(schedule_task_t<T> task) {
+    if (tasks.size() >= MAX_TASKS) {
+        Serial.println("Max tasks reached");
+        return false;
+    }
+    tasks.push_back(task);
+    return writeTaskToFile(&task);
+}
+
+template<typename T>
+bool Scheduler<T>::removeTask(uint8_t id) {
+    int8_t index = -1;
+    for (uint8_t i = 0; i < tasks.size(); i++) {
+        if (tasks[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index < 0) {
+        Serial.println("Task not found");
+        return false;
+    }
+
+    tasks.erase(tasks.begin() + index);
+    return save();
+}
+
+template<typename T>
+bool Scheduler<T>::updateTask(uint8_t id, schedule_task_t<T> task) {
+    for (auto t: tasks) {
+        if (t.id == id) {
+            t = task;
+            return save();
+        }
+    }
+    Serial.println("Task not found");
+    return false;
+}
+
+template<typename T>
+void Scheduler<T>::run() {
+    uint8_t h = timeClient->getHours();
+    uint8_t m = timeClient->getMinutes();
+    uint8_t dow = timeClient->getDay(); // 0 is sunday
+    bool anychange = false;
+
+    for (auto &task: tasks) {
+        if (task.enabled) {
+            if ((task.repeat.monday && dow == 1) ||
+                (task.repeat.tuesday && dow == 2) ||
+                (task.repeat.wednesday && dow == 3) ||
+                (task.repeat.thursday && dow == 4) ||
+                (task.repeat.friday && dow == 5) ||
+                (task.repeat.saturday && dow == 6) ||
+                (task.repeat.sunday && dow == 0)) {
+                if (task.time.hour == h && task.time.minute == m) {
+                    if (!task.executed) {
+                        task.executed = true;
+                        anychange = true;
+                        if (callback)
+                            callback(task);
+                    }
+                } else if (task.executed) {
+                    task.executed = false;
+                    anychange = true;
+                }
+            }
+        }
+    }
+
+    if (anychange) {
+        save();
+    }
+}
+
+template<typename T>
+schedule_task_t<T> Scheduler<T>::getTaskById(uint8_t id) {
+    for (auto task: tasks) {
+        if (task.id == id) {
+            return task;
+        }
+    }
+    return {};
+}
+
+template<typename T>
+uint8_t Scheduler<T>::getTaskCount() {
+    return tasks.size();
+}
+
+template<typename T>
+String Scheduler<T>::getString() {
+    openFile(false, READ_ONLY);
+    String result = file.readString();
+    closeFile();
+    if (result.length() == 0) {
+        result = "EMPTY";
+    }
+    return result;
+}
+
+template<typename T>
+void Scheduler<T>::printToSerial(Stream &stream) {
+    stream.printf("Task count: %d\n\n", tasks.size());
+    for (auto &task: tasks) {
+        stream.printf("Task %d: %02d:%02d\nrepeat: %d|%d|%d|%d|%d|%d|%d\nargs: %s\nenabled: %d\nexecuted: %d\n\n",
+                      task.id,
+                      task.time.hour,
+                      task.time.minute,
+                      task.repeat.monday,
+                      task.repeat.tuesday,
+                      task.repeat.wednesday,
+                      task.repeat.thursday,
+                      task.repeat.friday,
+                      task.repeat.saturday,
+                      task.repeat.sunday,
+                      task.args->toString().c_str(),
+                      task.enabled,
+                      task.executed);
+    }
+
+    Serial.println("======== Read from file ========");
+    openFile(false, READ_ONLY);
+
+    Serial.printf("File size: %d\n", file.size());
+    while (file.available()) {
+        Serial.print("[Task] ");
+        Serial.println(file.readStringUntil('\n'));
+    }
+
+    Serial.println("=========== End file ===========");
+    closeFile();
+}
---
+++ smart-garden/lib/Scheduler/Scheduler.hpp
@@ -0,0 +1,203 @@
+#pragma once
+
+#ifndef  SCHEDULER_HPP
+#define SCHEDULER_HPP
+
+#include <Arduino.h>
+#include <vector>
+#include <NTPClient.h>
+
+#if defined(ESP8266)
+#include <LittleFS.h>
+#define SCHEDULE_FS LittleFS
+#elif defined(ESP32)
+#include <SPIFFS.h>
+#define SCHEDULE_FS SPIFFS
+#endif
+
+class ScheduleTaskArgsBase;
+template <class T = ScheduleTaskArgsBase> class Scheduler;
+
+struct schedule_time_t
+{
+    uint8_t hour;
+    uint8_t minute;
+};
+
+struct schedule_repeat_t
+{
+    bool monday;
+    bool tuesday;
+    bool wednesday;
+    bool thursday;
+    bool friday;
+    bool saturday;
+    bool sunday;
+};
+
+template <typename T = ScheduleTaskArgsBase> struct schedule_task_t
+{
+    uint8_t id;
+    schedule_time_t time;
+    schedule_repeat_t repeat;
+    T* args; // extended from ScheduleTaskArgsBase
+    bool enabled;
+    bool executed;
+};
+
+
+/**
+ * @brief Base class for task arguments. extend this class to add more arguments to the task
+ *
+ */
+class ScheduleTaskArgsBase
+{
+public:
+    ScheduleTaskArgsBase() = default;
+
+    virtual void parse(String& args) { }
+
+    virtual String toString() { return "NULL"; }
+
+};
+
+
+
+template <typename T> class Scheduler
+{
+
+public:
+    String filePath = "/schedules.txt";
+    uint8_t MAX_TASKS = 10;
+    
+    explicit Scheduler(NTPClient *timeClient);
+    ~Scheduler();
+
+    /**
+     * @brief Add a new task to the schedule
+     * 
+     * @param task 
+     * @return true 
+     * @return false 
+     */
+    bool addTask(schedule_task_t<T> task);
+    
+    /**
+     * @brief Remove a task from the schedule by its id
+     * 
+     * @param id 
+     * @return true 
+     * @return false 
+     */
+    bool removeTask(uint8_t id);
+    
+    /**
+     * @brief Update a task in the schedule by its id
+     * 
+     * @param id 
+     * @param task new task to update
+     * @return true 
+     * @return false 
+     */
+    bool updateTask(uint8_t id, schedule_task_t<T> task);
+    
+    /**
+     * @brief Set the Callback object
+     * 
+     * @param callback 
+     */
+    void setCallback(std::function<void(schedule_task_t<T>)> callback);
+    
+    
+    /**
+     * @brief Get number of tasks in the schedule
+     * 
+     * @return uint8_t 
+     */
+    uint8_t getTaskCount();
+
+    /**
+     * @brief Load tasks from file
+     * 
+     */
+    bool load();
+
+    /**
+     * @brief Overwrite the file with the current tasks
+     * 
+     * @param overwrite 
+     * @return true 
+     * @return false 
+     */
+    bool save();
+
+    /**
+     * @brief Run the scheduler
+     * 
+     */
+    void run();
+
+    /**
+     * @brief Get the Task By task id
+     * 
+     * @param id 
+     * @return schedule_task_t 
+     */
+    schedule_task_t<T> getTaskById(uint8_t id);
+
+    /**
+     * @brief Print all tasks to serial
+     * 
+     * @param serial 
+     */
+    void printToSerial(Stream &stream);
+
+
+    /**
+     * @brief Get the String object
+     * 
+     * @return String 
+     */
+    String getString();
+
+
+    /**
+     * @brief Parse a task from string
+     * 
+     * @param task 
+     * @return schedule_task_t 
+     */
+    static schedule_task_t<T> parseTask(const String& task);
+
+
+private:
+    enum FileMode {
+        READ_ONLY = 0x00,
+        WRITE_ONLY = 0x01,
+        APPEND = 0x02,
+        READ_WRITE = 0x03,
+    };
+
+    std::vector<schedule_task_t<T>> tasks;
+    std::function<void(schedule_task_t<T>)> callback;
+    File file;
+    NTPClient *timeClient{};
+
+    void openFile(bool overwrite = false, uint8_t mode = READ_WRITE);
+    void closeFile();
+
+    /**
+     * @brief Append a task to file
+     * 
+     * @param file file object
+     * @param task task object
+     * @return true 
+     * @return false 
+     */
+    bool writeTaskToFile(schedule_task_t<T> *task);
+    
+};
+
+#include "Scheduler.cpp"
+
+#endif //SCHEDULER_HPP
