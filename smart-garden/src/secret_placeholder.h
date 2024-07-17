06d4df5de42ffe98a303c7cfd5b069b8dd54dbf9
Thái <62926491+nhthai173@users.noreply.github.com>
2024-07-17 23:32:35 +0700

    Add secret placeholder

---
+++ smart-garden/src/secret_placeholder.h
@@ -0,0 +1,28 @@
+/**
+ * This file is a placeholder for the actual secret values.
+ */
+
+#ifndef SMART_GARDEN_SECRET_PLACEHOLDER_H
+#define SMART_GARDEN_SECRET_PLACEHOLDER_H
+
+// WiFi
+// @TODO User can configure from web interface
+#define WIFI_SSID
+#define WIFI_PASSWORD
+
+// Telegram Bot
+#define BOT_TOKEN
+#define CHAT_ID
+
+// Firebase user
+// @TODO User can configure from web interface
+// @TODO store hashed password
+#define USER_EMAIL
+#define USER_PASSWORD
+
+// Database secret
+#define API_KEY
+#define DATABASE_URL
+
+
+#endif //SMART_GARDEN_SECRET_PLACEHOLDER_H
