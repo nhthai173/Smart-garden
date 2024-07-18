b15cc2e0e358b1e87fa7aaf4bba53137496d3d11
Thái <62926491+nhthai173@users.noreply.github.com>
2024-07-19 02:35:01 +0700

    Update

--- smart-garden/src/FirebaseIOT.h
+++ smart-garden/src/FirebaseIOT.h
@@ -5,8 +5,6 @@
 #ifndef SMART_GARDEN_FIREBASEIOT_H
 #define SMART_GARDEN_FIREBASEIOT_H
 
-#include "FirebaseClient.h"
-
 #define USE_STREAM
 #define FB_PRINT_RESULT
 
@@ -24,94 +22,13 @@ RealtimeDatabase Database;
 AsyncResult aResult_no_callback;
 
 #ifdef USE_STREAM
+
 WiFiClientSecure stream_ssl_client;
 AsyncClientClass streamClient(stream_ssl_client, getNetwork(fbNetwork));
-#endif
+
+#endif // USE_STREAM
 
 #ifdef FB_PRINT_RESULT
-
-void printResult(AsyncResult &aResult);
-
-#else
-void printResult(AsyncResult &aResult) {}
-#endif
-
-String DB_DEVICE_PATH = "";
-bool FB_FIRST_CONNECTED = false;
-std::function<void()> onFBFirstConnectedCB = nullptr;
-
-
-void onFBFirstConnected(std::function<void()> cb) {
-    onFBFirstConnectedCB = std::move(cb);
-}
-
-
-void fb_begin(const String &api, const String &db_url, const String &email, const String &password) {
-    ssl_client.setInsecure();
-
-#ifdef USE_STREAM
-    stream_ssl_client.setInsecure();
-#endif
-
-    user_auth = new UserAuth(api, email, password);
-
-    initializeApp(aClient, app, getAuth(*user_auth), printResult, "authTask");
-
-    // Binding the FirebaseApp for authentication handler.
-    // To unbind, use Database.resetApp();
-    app.getApp<RealtimeDatabase>(Database);
-    Database.url(db_url);
-
-    Database.setSSEFilters("get,put,patch,cancel");
-//    Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
-}
-
-
-template<typename T = object_t>
-void database_set(const String &path, const T &value) {
-    Database.set<T>(aClient, DB_DEVICE_PATH + path, value, aResult_no_callback);
-}
-
-template<typename T = object_t>
-void database_set(const String &path, const T &value, AsyncResultCallback callback) {
-    Database.set<T>(aClient, DB_DEVICE_PATH + path, value, callback);
-}
-
-template<typename T = object_t>
-void database_update(const String &path, const T &value) {
-    Database.update<T>(aClient, DB_DEVICE_PATH + path, value, aResult_no_callback);
-}
-
-template<typename T = object_t>
-void database_update(const String &path, const T &value, AsyncResultCallback callback) {
-    Database.update<T>(aClient, DB_DEVICE_PATH + path, value, callback);
-}
-
-void database_get(const String &path, AsyncResultCallback callback) {
-    Database.get(aClient, DB_DEVICE_PATH + path, callback);
-}
-
-void database_stream(const String &path, AsyncResultCallback callback) {
-    Database.get(aClient, DB_DEVICE_PATH + path, callback, true);
-}
-
-
-void fb_loop() {
-    app.loop();
-    Database.loop();
-
-    if (app.ready() && !FB_FIRST_CONNECTED) {
-        FB_FIRST_CONNECTED = true;
-
-        // Set Device Path
-        DB_DEVICE_PATH = app.getUid() + "/" + (String) ESP.getEfuseMac();
-
-        if (onFBFirstConnectedCB) {
-            onFBFirstConnectedCB();
-        }
-    }
-}
-
 
 void printResult(AsyncResult &aResult) {
     if (aResult.isEvent()) {
@@ -144,5 +61,97 @@ void printResult(AsyncResult &aResult) {
     }
 }
 
+#else
+
+void printResult(AsyncResult &aResult) {}
+
+#endif // FB_PRINT_RESULT
+
+
+class FirebaseIOTClass {
+
+private:
+    bool _firstConnected = false;
+    std::function<void()> _firstConnectedCB = nullptr;
+
+public:
+    String DB_DEVICE_PATH;
+
+    static void begin(const String &api, const String &db_url, const String &email, const String &password) {
+        ssl_client.setInsecure();
+
+#ifdef USE_STREAM
+        stream_ssl_client.setInsecure();
+#endif
+
+        user_auth = new UserAuth(api, email, password);
+
+        initializeApp(aClient, app, getAuth(*user_auth), printResult, "authTask");
+
+        // Binding the FirebaseApp for authentication handler.
+        // To unbind, use Database.resetApp();
+        app.getApp<RealtimeDatabase>(Database);
+        Database.url(db_url);
+
+        Database.setSSEFilters("get,put,patch,cancel");
+//    Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
+    }
+
+    void onFirstConnected(std::function<void()> cb) {
+        _firstConnectedCB = std::move(cb);
+    }
+
+    void loop() {
+        app.loop();
+        Database.loop();
+
+        if (app.ready() && !_firstConnected) {
+            _firstConnected = true;
+
+            DB_DEVICE_PATH = app.getUid() + "/" + (String) ESP.getEfuseMac();
+
+            if (_firstConnectedCB) {
+                _firstConnectedCB();
+            }
+        }
+    }
+
+    template<typename T = object_t>
+    void set(const String &path, const T &value) {
+        Database.set<T>(aClient, DB_DEVICE_PATH + path, value, aResult_no_callback);
+    }
+
+    template<typename T = object_t>
+    void set(const String &path, const T &value, AsyncResultCallback callback, const String &uid = "") {
+        Database.set<T>(aClient, DB_DEVICE_PATH + path, value, callback, uid);
+    }
+
+    template<typename T = object_t>
+    void update(const String &path, const T &value) {
+        Database.update<T>(aClient, DB_DEVICE_PATH + path, value, aResult_no_callback);
+    }
+
+    template<typename T = object_t>
+    void update(const String &path, const T &value, AsyncResultCallback callback, const String &uid = "") {
+        Database.update<T>(aClient, DB_DEVICE_PATH + path, value, callback, uid);
+    }
+
+    void get(const String &path, AsyncResultCallback callback, const String &uid = "") const {
+        Database.get(aClient, DB_DEVICE_PATH + path, callback, false, uid);
+    }
+
+#ifdef USE_STREAM
+
+    bool setStream(const String &path, AsyncResultCallback callback, const String &uid = "streamTask") const {
+        Database.get(streamClient, DB_DEVICE_PATH + path, callback, true, uid);
+        return true;
+    }
+
+#endif
+
+};
+
+
+static FirebaseIOTClass FirebaseIOT;
 
 #endif //SMART_GARDEN_FIREBASEIOT_H
--- smart-garden/src/main.cpp
+++ smart-garden/src/main.cpp
@@ -15,13 +15,35 @@
 
 
 // ayushsharma82/ElegantOTA@^3.1.2
- #define ENABLE_SERVER
+#define ENABLE_SERVER
 
 // mobizt/FirebaseClient@^1.3.5
 #define ENABLE_NFIREBASE
 
 // https://github.com/nhthai173//NTPClient.git
- #define ENABLE_NTP
+#define ENABLE_NTP
+
+//#define USE_LOGGER
+#define USE_SCHEDULER
+
+#if defined(ENABLE_NTP)
+#if defined(USE_LOGGER)
+#define ENABLE_LOGGER
+#endif
+#if defined(USE_SCHEDULER)
+#define ENABLE_SCHEDULER
+#endif
+#endif
+
+
+#define DEVICE_NAME "Watering System"
+#define DEVICE_VERSION "0.3.0"
+#define FIRMWARE_VERSION 45
+
+#define FLOW_SENSOR_PIN 35
+#define VOLTAGE_PIN 32
+
+#define OUTPUT_ACTIVE_STATE LOW
 
 
 #include <Arduino.h>
@@ -29,12 +51,16 @@
 #include <SimpleTimer.h>
 #include "secret.h" // see secret_placeholder.h
 
+#include "GenericOutput.h"
+#include "GenericInput.h"
+#include "VirtualOutput.h"
+#include "VoltageReader.h"
+
 #if defined(ENABLE_NTP)
+
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 
-#include "WateringSchedule.h"
-#include "SLog.h"
 #endif // ENABLE_NTP
 
 #if defined(ENABLE_SERVER)
@@ -47,29 +73,20 @@
 
 
 #if defined(ENABLE_NFIREBASE)
+
 #include "FirebaseIOT.h"
+#include "FirebaseRTDBIntegrate.h"
+
+fbrtdb_object *RTDBObj;
+
 #endif // ENABLE_NFIREBASE
 
-
-#include "GenericOutput.h"
-#include "GenericInput.h"
-#include "VirtualOutput.h"
-#include "VoltageReader.h"
-
-#define DEVICE_NAME "Watering System"
-#define DEVICE_VERSION "0.3.0"
-#define FIRMWARE_VERSION 40
-
-#define FLOW_SENSOR_PIN 35
-#define VOLTAGE_PIN 32
-
-#define OUTPUT_ACTIVE_STATE LOW
 
 GenericOutput ValvePower(19, OUTPUT_ACTIVE_STATE, 8000L /* 8 sec */); // R1
 GenericOutput ValveDirection(18, OUTPUT_ACTIVE_STATE); // R2
 GenericOutput PumpPower(16, OUTPUT_ACTIVE_STATE); // R3
 GenericOutput ACPower(4, OUTPUT_ACTIVE_STATE, 180000L /* 3 min */); // R4
-VirtualOutput Valve(true, 60000L /* 1 min */);
+VirtualOutput Valve(60000L /* 1 min */);
 GenericInput WaterLeak(34, INPUT_PULLUP, LOW);
 VoltageReader PowerVoltage(32, 10.0, 3.33, 0.3, 10.5, 13.5);
 
@@ -81,10 +98,24 @@ SimpleTimer timer;
 WiFiUDP ntpUDP;
 NTPClient timeClient = NTPClient(ntpUDP, "pool.ntp.org", 7 * 3600);
 
-Scheduler<WateringTaskArgs> scheduler(&timeClient);
+#if defined(ENABLE_SCHEDULER)
+
+#include "WateringSchedule.h"
+
+void WateringTaskExec(schedule_task_t<WateringTaskArgs> task);
+
+Scheduler<WateringTaskArgs> scheduler(&timeClient, WateringTaskExec);
+
+#endif // ENABLE_SCHEDULER
+
+#if defined(ENABLE_LOGGER)
+
+#include "SLog.h"
 //SLog logger(&timeClient, BOT_TOKEN, CHAT_ID);
 SLog logger(&timeClient);
-#endif
+
+#endif // ENABLE_LOGGER
+#endif // ENABLE_NTP
 
 
 #if defined(ENABLE_SERVER)
@@ -152,7 +183,7 @@ WSHandler(AsyncWebSocket *sv, AsyncWebSocketClient *client, AwsEventType type, v
     switch (type) {
         case WS_EVT_CONNECT:
             Serial.printf("WS client [%d] connected\n", client->id());
-            notifyState();
+            timer.setTimeout(100L, notifyState);
             break;
         case WS_EVT_DISCONNECT:
             Serial.printf("WS client [%d] disconnected\n", client->id());
@@ -173,7 +204,9 @@ WSHandler(AsyncWebSocket *sv, AsyncWebSocketClient *client, AwsEventType type, v
                 } else {
                     Valve.close();
                 }
+#if defined(ENABLE_LOGGER)
                 logger.log("VALVE_" + message.substring(6), "WEB", client->remoteIP().toString());
+#endif
             }
             break;
         case WS_EVT_PONG:
@@ -188,6 +221,26 @@ WSHandler(AsyncWebSocket *sv, AsyncWebSocketClient *client, AwsEventType type, v
 #endif
 
 
+
+#if defined(ENABLE_NFIREBASE)
+
+void syncRTDB() {
+    JsonWriter writer;
+    object_t o1, o2, o3, o4, o5, o6, json;
+    writer.create(o1, "valve", Valve.getState());
+    writer.create(o2, "r1", ValvePower.getState());
+    writer.create(o3, "r2", ValveDirection.getState());
+    writer.create(o4, "r3", PumpPower.getState());
+    writer.create(o5, "r4", ACPower.getState());
+    writer.create(o6, "water_leak", WaterLeak.getState());
+    writer.join(json, 6, o1, o2, o3, o4, o5, o6);
+    FirebaseIOT.update("/data", json);
+}
+
+#endif
+
+
+
 void mainLoop() {
     ValvePower.loop();
     ACPower.loop();
@@ -196,17 +249,23 @@ void mainLoop() {
     PowerVoltage.loop();
 #if defined(ENABLE_NTP)
     timeClient.update();
+#if defined(ENABLE_SCHEDULER)
     scheduler.run();
+#endif
+#if defined(ENABLE_LOGGER)
     logger.loop();
+#endif
 #endif
 }
 
-#if defined(ENABLE_NTP)
+
+
+#if defined(ENABLE_SCHEDULER)
 /**
  * Set a test schedule
  * @return
  */
-String setTestSchedule(uint8_t id, uint8_t hour, uint8_t min) {
+bool setTestSchedule(uint8_t id, uint8_t hour, uint8_t min) {
     if (scheduler.getTaskById(id).id) return "Schedule already set";
     schedule_task_t<WateringTaskArgs> task = {
             .id = id,
@@ -227,24 +286,45 @@ String setTestSchedule(uint8_t id, uint8_t hour, uint8_t min) {
             .enabled = true,
             .executed = false
     };
-    return scheduler.addTask(task) ? "Schedule set" : "Failed to set schedule";
+    return scheduler.addTask(task);
 }
 
-String removeSchedule(uint8_t id) {
-    return scheduler.removeTask(id) ? "Schedule removed" : "Failed to remove schedule";
-}
-
-#endif // ENABLE_NTP
-
-
-#if defined(ENABLE_NTP)
 /**
  * @brief Execute a watering task from schedule
  *
  * @param task
  */
-void WateringTaskExec(schedule_task_t<WateringTaskArgs>);
+void WateringTaskExec(schedule_task_t<WateringTaskArgs> task) {
+    if (task.args->waterLiters == 0 && task.args->duration == 0) {
+        return;
+    }
+
+    // @TODO implement for litters amount
+    if (task.args->waterLiters > 0) {
+    }
+
+    if (task.args->duration > 0) {
+        Valve.open(task.args->duration * 60000L);
+    } else {
+        // open with default duration
+        Valve.open();
+    }
+
+    // if specified, open the valve to a certain level
+    if (task.args->valveOpenLevel > 0) {
+        ValvePower.off();
+        delay(1000);
+        ValvePower.on(task.args->valveOpenLevel * 10);
+    }
+#if defined(ENABLE_LOGGER)
+    logger.log("VALVE_OPEN", "SCHEDULE", task.args->toString());
 #endif
+}
+
+#endif // ENABLE_SCHEDULER
+
+
+
 
 void setup() {
     Serial.begin(115200);
@@ -293,7 +373,7 @@ void setup() {
     });
     Valve.onPowerChanged(notifyState);
     Valve.onAutoOff([]() {
-#if defined(ENABLE_NTP)
+#if defined(ENABLE_LOGGER)
         logger.log("VALVE_CLOSE", "AUTO_TIMEOUT", "");
 #endif
     });
@@ -310,22 +390,18 @@ void setup() {
     });
     WaterLeak.onHoldState(true, 5000L, []() {
         Valve.close();
-#if defined(ENABLE_NTP)
+#if defined(ENABLE_LOGGER)
         logger.log("WATER_LEAK", "SENSOR", WaterLeak.getStateString());
         logger.log("VALVE_CLOSE", "WATER_LEAK", "");
         logger.logTele("🚨 Phát hiện ngập nước");
 #endif
     });
     WaterLeak.onHoldState(false, 5000L, []() {
-#if defined(ENABLE_NTP)
+#if defined(ENABLE_LOGGER)
         logger.log("WATER_LEAK", "SENSOR", WaterLeak.getStateString());
         logger.logTele("🚰 Nước đã hết ngập");
 #endif
     });
-
-#if defined(ENABLE_NTP)
-    scheduler.setCallback(WateringTaskExec);
-#endif
 
 #if defined(ENABLE_SERVER)
     server.onNotFound([](AsyncWebServerRequest *request) { request->send(404, "text/plain", "Not found"); });
@@ -345,8 +421,10 @@ void setup() {
 
     server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(200, "text/plain", "Reseting...");
+#if defined(SPIFFS_H) && (defined(ENABLE_LOGGER) || defined(ENABLE_SCHEDULER))
         SPIFFS.format();
         SPIFFS.end();
+#endif
         ESP.restart();
     });
 
@@ -358,23 +436,33 @@ void setup() {
     server.on("/watering", HTTP_ANY, [](AsyncWebServerRequest *request) {
         if (request->hasParam("stop")) {
             Valve.close();
+#if defined(ENABLE_LOGGER)
             logger.log("VALVE_CLOSE", "API", request->client()->remoteIP().toString());
+#endif
         } else {
             Valve.open();
+#if defined(ENABLE_LOGGER)
             logger.log("VALVE_OPEN", "API", request->client()->remoteIP().toString());
+#endif
         }
         request->send(200, "text/plain", "OK");
     });
 
-
+#if defined(ENABLE_LOGGER)
     server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(200, "text/plain", logger.getLogs());
     });
+#endif
 
+#if defined(ENABLE_SCHEDULER)
     server.on("/schedules", HTTP_GET, [](AsyncWebServerRequest *request) {
+#if defined(ENABLE_NFIREBASE)
+        request->send(200, "text/plain", String(scheduler.getTaskCount()));
+#else
         String ret = "Object task count: " + String(scheduler.getTaskCount()) + "\n";
         ret += "Read from file:\n" + scheduler.getString();
         request->send(200, "text/plain", ret);
+#endif
     });
 
     server.on("/test-schedule", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -392,7 +480,11 @@ void setup() {
             request->send(400, "text/plain", "Invalid id");
             return;
         }
-        request->send(200, "text/plain", setTestSchedule(id, hour, min));
+        if (setTestSchedule(id, hour, min)) {
+            request->send(200, "text/plain", "Schedule added");
+        } else {
+            request->send(400, "text/plain", "Failed to add schedule");
+        }
     });
 
     server.on("/remove-schedule", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -404,9 +496,15 @@ void setup() {
             request->send(400, "text/plain", "Invalid id");
             return;
         }
-        request->send(200, "text/plain", removeSchedule(id));
+        if (scheduler.removeTask(id)) {
+            request->send(200, "text/plain", "Removed schedule");
+        } else {
+            request->send(400, "text/plain", "Failed to remove schedule");
+        }
     });
+#endif
 
+#if defined(ENABLE_LOGGER)
     ElegantOTA.onEnd([](bool isSuccess) {
         if (isSuccess) {
             logger.log("FIRMWARE_UPDATE", "WEB", String(FIRMWARE_VERSION));
@@ -414,6 +512,7 @@ void setup() {
             logger.log("FIRMWARE_UPDATE_FAILED", "WEB", String(FIRMWARE_VERSION));
         }
     });
+#endif
 
     /* ===================== */
 
@@ -426,39 +525,75 @@ void setup() {
 #if defined(ENABLE_NTP)
     timeClient.begin();
     timeClient.update();
-
+#if defined(ENABLE_LOGGER)
     logger.clearOldLogs();
     logger.log("START", "SYSTEM", String(FIRMWARE_VERSION));
-#endif
+#endif // ENABLE_LOGGER
+#endif // ENABLE_NTP
 
     timer.setInterval(500L, mainLoop);
 
 
 #if defined(ENABLE_NFIREBASE)
-    timer.setInterval(250L, fb_loop);
+    RTDBObj = new fbrtdb_object();
+    RTDBObj->client = &aClient;
+    RTDBObj->rtdb = &Database;
 
-    fb_begin(API_KEY, DATABASE_URL, USER_EMAIL, USER_PASSWORD);
-    onFBFirstConnected([](){
+    // The path should be set after user is authenticated
+    // <user_id>/<device_id>/data
+    RTDBObj->prefixPath = FirebaseIOT.DB_DEVICE_PATH + "/data";
+
+    Valve.attachDatabase(RTDBObj, "/valve", stdGenericOutput::START_UP_OFF, printResult);
+    ValvePower.attachDatabase(RTDBObj, "/r1", stdGenericOutput::START_UP_OFF, printResult);
+    ValveDirection.attachDatabase(RTDBObj, "/r2", stdGenericOutput::START_UP_OFF, printResult);
+    PumpPower.attachDatabase(RTDBObj, "/r3", stdGenericOutput::START_UP_OFF, printResult);
+    ACPower.attachDatabase(RTDBObj, "/r4", stdGenericOutput::START_UP_OFF, printResult);
+
+#if defined(ENABLE_SCHEDULER)
+    scheduler.attachDatabase(RTDBObj);
+#endif
+
+    timer.setInterval(250, [](){
+        FirebaseIOT.loop();
+    });
+
+    FirebaseIOT.begin(API_KEY, DATABASE_URL, USER_EMAIL, USER_PASSWORD);
+    FirebaseIOT.onFirstConnected([]() {
+        // Update path
+        RTDBObj->prefixPath = FirebaseIOT.DB_DEVICE_PATH + "/data";
+        scheduler.setPath(FirebaseIOT.DB_DEVICE_PATH + "/schedules");
+
         // Set info
-        database_update("/info", (object_t)getInfo());
+        FirebaseIOT.update("/info", (object_t) getInfo());
+
+        // Test schedule
+//        JsonWriter writer;
+//        object_t arr;
+//        arr.initArray();
+//        writer.join(arr, 2 /* no. of object_t (s) to join */,
+//                    object_t(string_t("1|7|0|1111111|10-0-1|0|0")),
+//                    object_t(string_t("2|10|30|1010101|10-0-1|1|0")));
+//        arr.printTo(Serial);
+//        FirebaseIOT.set<object_t>("/schedules", arr, printResult);
+
+//        FirebaseIOT.get("/schedules", [](AsyncResult &res){
+//            printResult(res);
+//            JSON::JsonParser parser(res.payload());
+//            Serial.printf("Item0: %s\n", parser.getItem(0).c_str());
+//            Serial.printf("Item1: %s\n", parser.getItem(1).c_str());
+//        });
+
+        scheduler.load();
+
 
         // Set stream
-        database_stream("/data", [](AsyncResult &res){
+        FirebaseIOT.setStream("/data", [](AsyncResult &res) {
             printResult(res);
 
             auto &RTDB = res.to<RealtimeDatabaseResult>();
             // First time connect -> intit data
             if (RTDB.event() == "put" && RTDB.dataPath() == "/" && RTDB.type() == realtime_database_data_type_null) {
-                JsonWriter writer;
-                object_t o1, o2, o3, o4, o5, o6, json;
-                writer.create(o1, "valve", Valve.getState());
-                writer.create(o2, "r1", ValvePower.getState());
-                writer.create(o3, "r2", ValveDirection.getState());
-                writer.create(o4, "r3", PumpPower.getState());
-                writer.create(o5, "r4", ACPower.getState());
-                writer.create(o6, "water_leak", WaterLeak.getState());
-                writer.join(json, 6, o1, o2, o3, o4, o5, o6);
-                database_set("/data", json);
+                syncRTDB();
             } else if (RTDB.dataPath() == "/" && RTDB.type() == realtime_database_data_type_json) {
                 JSON::JsonParser parser(RTDB.to<String>());
                 Valve.setState(parser.getBool("valve"));
@@ -469,7 +604,7 @@ void setup() {
                 notifyState();
 
                 if (parser.getBool("water_leak") != WaterLeak.getState()) {
-                    database_set("/data/water_leak", WaterLeak.getState());
+                    FirebaseIOT.set("/data/water_leak", WaterLeak.getState());
                 }
             } else if (RTDB.dataPath() == "/r1") {
                 ValvePower.setState(RTDB.to<bool>());
@@ -482,7 +617,7 @@ void setup() {
             } else if (RTDB.dataPath() == "/valve") {
                 Valve.setState(RTDB.to<bool>());
             } else if (RTDB.dataPath() == "/water_leak" && RTDB.to<bool>() != WaterLeak.getState()) {
-                database_set("/data/water_leak", WaterLeak.getState());
+                FirebaseIOT.set("/data/water_leak", WaterLeak.getState());
             }
 
             notifyState();
@@ -524,9 +659,14 @@ bool connectWiFi() {
     return true;
 }
 
-#if defined(ENABLE_SERVER)
 
 void notifyState() {
+
+#if defined(ENABLE_NFIREBASE)
+
+#endif
+
+#if defined(ENABLE_SERVER)
     if (ws.getClients().empty()) return;
     String message = "R1:" + ValvePower.getStateString() + "\n";
     message += "R2:" + ValveDirection.getStateString() + "\n";
@@ -535,42 +675,12 @@ void notifyState() {
     message += "VALVE:" + Valve.getStateString() + "\n";
     message += "WATER_LEAK:" + WaterLeak.getStateString() + "\n";
     ws.textAll(message);
-}
-
-#else
-
-void notifyState() {}
-
 #endif
 
-
-#if defined(ENABLE_NTP)
-void WateringTaskExec(schedule_task_t<WateringTaskArgs> task) {
-    if (task.args->waterLiters == 0 && task.args->duration == 0) {
-        return;
-    }
-
-    // @TODO implement for litters amount
-    if (task.args->waterLiters > 0) {
-    }
-
-    if (task.args->duration > 0) {
-        Valve.open(task.args->duration * 60000L);
-    } else {
-        // open with default duration
-        Valve.open();
-    }
-
-    // if specified, open the valve to a certain level
-    if (task.args->valveOpenLevel > 0) {
-        ValvePower.off();
-        delay(1000);
-        ValvePower.on(task.args->valveOpenLevel * 10);
-    }
-
-    logger.log("VALVE_OPEN", "SCHEDULE", task.args->toString());
 }
-#endif // ENABLE_NTP
+
+
+
 
 
 #if defined(ENABLE_NFIREBASE)
